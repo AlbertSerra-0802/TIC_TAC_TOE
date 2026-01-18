8051101314a47aa4184e53216a385733dcc82517
AlbertSerra-0802 <aserrav@esliceu.net>
2026-01-18 13:10:05 +0100

    dia 18 .js

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 let btnRef = document.querySelectorAll(".button-option");
 let popupRef = document.querySelector(".popup");
+let newgameBtn = document.getElementaryByld("new-game");
+let restartBtn = document.getElementById("restar");
